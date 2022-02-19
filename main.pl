505e79e3ef5bd64fffd6c70c7585c9eaba59eb7b
Romanov Andrey <andrevvroman@gmail.com>
2022-02-19 12:25:53 +0700

    Add usage of weekDay to timeDIf
    Also added more comments

--- main.pl
+++ main.pl
@@ -1,4 +1,4 @@
-:- dynamic nextDay/2, flightTime/3, flightDay/2, flight/3.
+:- dynamic weekDay/2, nextDay/2, flightTime/3, flightDay/2, flight/3.
 :- use_module(library(date)).
 
 isLeftMoreThanRight(Left,Right):- Left @> Right, true, ! ; false, !.
@@ -23,16 +23,18 @@ timediff(DateTime1, DateTime2, Sec) :-
   date_time_stamp(DateTime2, TimeStamp2),
   Sec is TimeStamp2 - TimeStamp1.
 
-timeDif(weekDay(RightD),h_m(RightH,RightM),weekDay(LeftD),h_m(LeftH,LeftM),Result):-
-  date_time_stamp(date(1970,01,LeftD,LeftH,LeftM,0,0,-,-), Stamp1),
-  date_time_stamp(date(1970,01,RightD,RightH,RightM,0,0,-,-), Stamp2),
+timeDif(RightD,h_m(RightH,RightM),LeftD,h_m(LeftH,LeftM),Result):-
+  weekDay(RightD,RightDInt),
+  weekDay(LeftD,LeftDInt),
+  date_time_stamp(date(1970,01,LeftDInt,LeftH,LeftM,0,0,-,-), Stamp1),
+  date_time_stamp(date(1970,01,RightDInt,RightH,RightM,0,0,-,-), Stamp2),
   Result is (Stamp1 - Stamp2)/3600,!.
 
 start:-
   % findAllFlights(london,dublin,mon,7,30,X),
   % writeln(X),
-  timeDif(weekDay(1),h_m(23,30),weekDay(2),h_m(05,45),X),
-  X < 5,
+  timeDif(mon,h_m(23,30),tue,h_m(05,45),X),
+  X < 7,
   writeln("Just in time"), !; write("Too late"),!.
   
 
@@ -47,17 +49,17 @@ route(From,To,Day,DepartTime,Answer):-
 route(From,To,Answer,_Path,FlightNumbers,CurrentDay,CurrentTime):-
   flight(FlightNumber,From,To), % Существует прямой рейс
   flightDay(FlightNumber,ScheduleDays),
-  member(CurrentDay,ScheduleDays),
+  member(CurrentDay,ScheduleDays), % Летает в текущий день
   flightTime(FlightNumber,ScheduleDepartTime,_),
-  isLeftMoreThanRight(ScheduleDepartTime,CurrentTime),
+  isLeftMoreThanRight(ScheduleDepartTime,CurrentTime), % Успеваем сесть на прямой рейс
   reverse([FlightNumber|FlightNumbers],Answer).
   
 route(From,To,Answer,Path,FlightNumbers,CurrentDay,CurrentTime):-
   flight(FlightNumber, From, Transfer),
   not(member(Transfer,Path)),% Если мы еще не были в этом городе
   flightDay(FlightNumber,ScheduleDays),
-  member(CurrentDay,ScheduleDays),
-  flightTime(FlightNumber,ScheduleDepartTime,ScheduleArriveTime),% проверка совпадения дня отправления с текущим
+  member(CurrentDay,ScheduleDays), % Летает в текущий день
+  flightTime(FlightNumber,ScheduleDepartTime,ScheduleArriveTime),
   isLeftMoreThanRight(ScheduleDepartTime,CurrentTime),% проверка успеваем ли мы на начало рейса
   changeDayIfNeed(ScheduleDepartTime,ScheduleArriveTime,CurrentDay,NewDay),% меняем день недели, если рейс ночной
   route(Transfer,To,Answer,[Transfer|Path],[FlightNumber|FlightNumbers],NewDay,ScheduleArriveTime).%Ищем путь с пересадкой от этого города
